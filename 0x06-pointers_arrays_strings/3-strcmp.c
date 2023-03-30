afcfbe069cb6b91ecbe43afdeadadce228ffa15e
Neville256 <nsoundy796@gmail.com>
2023-03-30 07:37:44 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
   *_strcmp - compare string values
   * @s1: input value
--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 /**
  * main - check the code for
- *
  * Return: Always 0.
  */
 int main(void)
